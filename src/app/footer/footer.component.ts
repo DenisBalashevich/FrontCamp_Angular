3e96e246e01cd91bd3979817e46e92035527b930
Denis Balashevich <Denis_Balashevich@epam.com>
2020-01-13 14:29:27 +0300

    Initial

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+    selector: 'footer-element',
+    templateUrl: './footer.component.html',
+})
+export class FooterElementComponent {
+}
